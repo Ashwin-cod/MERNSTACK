26cc06d6e205fb85accea82473807c8148415a62
Ashwin <ashwinrj453@gmail.com>
2025-10-16 20:16:22 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+age=18;
+console.log(age)
+function  sayhello() {
+    var name="Ashwin";
+    name="rj";
+    console.log("Hello" + name);
+}
+sayhello(name);
+console.log("hello outside" + name);
+nature="rain";
+console.log(nature)
+function greet(name,age){
+    return "hello"+ name +"your age is"+ age;
+};
+console.log(greet("Dinesh",25));
